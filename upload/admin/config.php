d5fc8e721898525ee560da697eef7ea11ae8da0f
Joachim Engelhardt <joachim_engelhardt@web.de>
2014-08-02 21:28:27 +0200

    embed in eclipse PHP project renamed / configured config files

---
+++ upload/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\opencart\upload/admin/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\opencart\upload/system/');
+define('DIR_DATABASE', 'C:\xampp\htdocs\opencart\upload/system/database/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\opencart\upload/admin/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\opencart\upload/admin/view/template/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\opencart\upload/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\opencart\upload/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\opencart\upload/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\opencart\upload/download/');
+define('DIR_LOGS', 'C:\xampp\htdocs\opencart\upload/system/logs/');
+define('DIR_CATALOG', 'C:\xampp\htdocs\opencart\upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'oc_user');
+define('DB_PASSWORD', 'cSVcwhtbUBdeXs88');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
